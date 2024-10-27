b323bc1ae996fd3ceb508aa9e6121838a981ff87
kubil6y <lieqb2@gmail.com>
2024-10-27 17:37:14 +0300

    59

--- src/Application.cpp
+++ src/Application.cpp
@@ -12,7 +12,7 @@ void Application::Setup() {
     smallBall->radius = 4;
     particles.push_back(smallBall);
 
-    Particle* bigBall = new Particle(50, 200, 3.f);
+    Particle* bigBall = new Particle(150, 100, 3.f);
     bigBall->radius = 12;
     particles.push_back(bigBall);
 }
@@ -51,26 +51,33 @@ void Application::Update() {
         particle->AddForce(wind);
     }
 
+    // Apply gravity force
+    for (auto particle : particles) {
+        Vec2 grativy{0.f, particle->mass * 9.81f * PIXELS_PER_METER};
+        particle->AddForce(grativy);
+    }
+
     for (auto particle : particles) {
         particle->Integrate(deltaTime);
     }
 
+    // Bounds correction
     for (auto particle : particles) {
-        // Keep in boundaries
         if (particle->position.x - particle->radius <= 0) {
             particle->position.x = particle->radius;
-            particle->velocity.x *= -0.9f;
+            particle->velocity.x *= -0.9;
         } else if (particle->position.x + particle->radius >=
                    Graphics::Width()) {
             particle->position.x = Graphics::Width() - particle->radius;
-            particle->velocity.x *= -0.9f;
-        } else if (particle->position.y - particle->radius <= 0) {
+            particle->velocity.x *= -0.9;
+        }
+        if (particle->position.y - particle->radius <= 0) {
             particle->position.y = particle->radius;
-            particle->velocity.y *= -0.9f;
+            particle->velocity.y *= -0.9;
         } else if (particle->position.y + particle->radius >=
                    Graphics::Height()) {
             particle->position.y = Graphics::Height() - particle->radius;
-            particle->velocity.y *= -0.9f;
+            particle->velocity.y *= -0.9;
         }
     }
 }
